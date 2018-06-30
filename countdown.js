df39d1fd565f6fd91c5675f321f0a47078db5cfe
tedshd <tedshd@gmail.com>
2018-06-30 15:14:50 +0800

    [dev] Modify the way call count.

--- countdown.js
+++ countdown.js
@@ -3,15 +3,18 @@ function countdown(time, dosomething) {
         console.erroe('not set time');
         return;
     }
+
     var count = new Date().getTime() + time*1000;
-    var tmp = setInterval(function () {
-        var currentTime = new Date().getTime();
-        var t = count - currentTime;
+    var init = setTimeout(function() {
+        var currentTime = new Date().getTime(),
+            t = count - currentTime,
+            tmp = setTimeout(arguments.callee, 1000);
         if (dosomething) {
             dosomething(--time);
         }
         if (t < 0) {
-            clearInterval(tmp);
+            clearTimeout(tmp);
+            clearTimeout(init);
         }
     }, 1000);
 }
